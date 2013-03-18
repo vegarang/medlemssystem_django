7a206a9873df035a5b16caac16790aec6c5a58e8
Vegard Angell <vegard@xangell.com>
2013-03-18 10:53:31 +0100

    added documentation in core/views.

--- core/views.py
+++ core/views.py
@@ -7,6 +7,12 @@ import datetime
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
 def delete_person(request):
+    """
+    Takes a POST-request with name (required) and email (optional), locates the person that matches, and deletes it. If no person is found, or multiple
+    people are found, an error will be returned.
+
+    if the argument 'check' is found in the POST-request, then a site asking if you are sure you want to delete is shown instead.
+    """
     if not request.POST:
         return render_to_response('core/delete.html', context_instance=RequestContext(request))
 
@@ -31,6 +37,13 @@ def delete_person(request):
     return render_to_response('core/add.html', {'removed':request.POST['name']}, context_instance=RequestContext(request))
 
 def add_person(request):
+    """
+    reads name (required) email (optional) and lifetime(optional) from a POST-request, and creates a Person with the required fields.
+
+    automatically locates or creates a Semester based on current date.
+
+    If a person with the same parameters already exist, an error will be shown on add-page.
+    """
     if not request.POST:
         return render_to_response('core/add.html', context_instance=RequestContext(request))
 
@@ -62,6 +75,11 @@ def add_person(request):
     return render_to_response('core/add.html', {'name':args['name']}, context_instance=RequestContext(request))
 
 def edit_person(request):
+    """
+    locates a person based on name and email in POST, and updates it with values new_name, new_email and life from POST.
+
+    error is shown if new_name == "" or an error occures when saving.
+    """
     if not 'name' in request.POST:
         return render_to_response('core/add.html', {'fail':'Cannot find person to edit.'}, context_instance=RequestContext(request))
 
@@ -78,42 +96,76 @@ def edit_person(request):
     p.email=request.POST['new_email']
     p.lifetime=True if request.POST['life'] == "life" else False
 
-    p.save()
+    try:
+        p.save()
+    except:
+        return render_to_response('core/add.html', {'fail':'Database-error, most likely name already exists..'}, context_instance=RequestContext(request))
     return render_to_response('core/add.html', {'name':p.name}, context_instance=RequestContext(request))
 
 
 def list_person(request):
+    """
+    lists all people, and gives info on how many members last 12 hours, and how many lifetime members.
+    """
     gap=datetime.datetime.now()-datetime.timedelta(hours=12)
     people=Person.objects.all()
     lifetime=len(people.filter(lifetime=True))
     current=len(people.filter(date_join__gte=gap))
     return render_to_response('core/list.html', {'people':people, 'total':len(people), 'num_cur':current, 'num_life':lifetime, 'headline':'All members'}, context_instance=RequestContext(request))
 
+def list_current(request):
+    """
+    same as list_person, but only lists people who are members this semester, or are lifetime members.
+    """
+    gap=datetime.datetime.now()-datetime.timedelta(hours=12)
+    now=datetime.date.today()
+    s=Semester.objects.filter(start_date__lte=now, end_date__gte=now)[0]
+    people=Person.objects.all().exclude(lifetime=False, date_join__lte=s.start_date)
+    lifetime=len(people.filter(lifetime=True))
+    current=len(people.filter(date_join__gte=gap))
+    return render_to_response('core/list.html', {'people':people, 'total':len(people), 'num_cur':current, 'num_life':lifetime, 'headline':'All members'}, context_instance=RequestContext(request))
+
 def list_lifetime(request):
+    """
+    lists only lifetime members
+    """
     people=Person.objects.filter(lifetime=True)
     return render_to_response('core/list.html', {'people':people, 'total':len(people), 'headline':'Lifetime members'}, context_instance=RequestContext(request))
 
 def makesemester():
+    """
+    make a new semester based on current date.
+    spring: jan 1 -> jun 30
+    fall: jul 1 -> dec 31
+    """
     now=datetime.date.today()
     tmp=datetime.date(now.year, 7, 1)
 
-    if now>tmp:
+    if now>=tmp:
         s=Semester.objects.create(name='h_{}'.format(now.year), start_date=tmp, end_date=datetime.date(now.year, 12, 31))
         return s
 
-    s=Semester.objects.create(name='v_{}'.format(now.year), start_date=datetime.date(now.year, 1, 1), end_date=tmp)
+    s=Semester.objects.create(name='v_{}'.format(now.year), start_date=datetime.date(now.year, 1, 1), end_date=(tmp-datetime.timedelta(days=1)))
     return s
 
 def index(request):
+    """
+    show main page
+    """
     return render_to_response('core/index.html')
 
 def search(request):
+    """
+    return a list of all people matching name in POST.
+
+    if membership is in post, and POST[membership] is 'lifetime' only lifetime members will be searched.
+    """
     if not request.POST:
         return render_to_response('core/search.html', {}, context_instance=RequestContext(request))
 
     people=Person.objects.filter(name__icontains=request.POST['name'])
 
-    if request.POST['membership'] == 'all':
+    if not 'membership' in request.POST or request.POST['membership'] == 'all':
         now=datetime.date.today()
         s=Semester.objects.filter(start_date__lte=now, end_date__gte=now)[0]
         people=people.exclude(lifetime=False, date_join__lte=s.start_date)
